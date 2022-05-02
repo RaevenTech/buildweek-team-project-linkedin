8b348eb34e7e7e25bf53d5ebd24d49abc6aab2fe
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-05-02 17:45:19 +0300

    Update App.js

--- linkedin-project/src/App.js
+++ linkedin-project/src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
 import "./App.css";
-import Analytics from "./components/Analytics/Analytics";
-import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
-        <div>
-            <Analytics />
-            <Footer />
-        </div>
+        <div>App</div>
     )
 }
 
