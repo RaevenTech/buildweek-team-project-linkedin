64599251571e583f0e4b52de34fc691a8fa6f823
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-04 16:39:23 +0100

    Update Layout.js

--- linkedin-project/src/components/layout/Layout.js
+++ linkedin-project/src/components/layout/Layout.js
@@ -6,29 +6,26 @@ import classes from "./Layout.module.css";
 import Header from "../navbar/Header";
 import Footer from "../Footer/Footer";
 
-
-
 export default function Layout() {
     return (
-        <Container>
-            <Row className={classes.main}>
-                <Col md={9} className={classes.hero}>
-                    <MyProfile />
-                </Col>
-                <Col md={3}>
-                    <h3> side bar </h3>
-                </Col>
-                <Row>
-                    <Col xs={3} className={classes.sidebar}>
-                        <h3> cards</h3>
+        <>
+            <Container>
+                <Header />
+                <Row className={classes.main}>
+                    <Col md={9} className={classes.hero}>
+                        <MyProfile />
                     </Col>
+                    <Col md={3}>
+                        <h3> side bar </h3>
+                    </Col>
+                    <Row>
+                        <Col xs={3} className={classes.sidebar}>
+                            <h3> cards</h3>
+                        </Col>
+                    </Row>
                 </Row>
-
             </Container>
             <Footer />
         </>
-
-      
-
     );
 }
