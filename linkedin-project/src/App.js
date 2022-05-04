fe5b932deecf3746bb853668359586f5bff35cdb
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-05-04 17:34:28 +0300

    a

--- linkedin-project/src/App.js
+++ linkedin-project/src/App.js
@@ -1,7 +1,7 @@
 //import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./components/layout/Layout";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
