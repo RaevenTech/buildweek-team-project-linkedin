07b0d3ae0e5c8d8eed0404f7becfee3b82ea2253
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-05-05 15:02:09 +0300

    starting

---
+++ linkedin-project/src/components/SmolProfile/PartTwo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import classes from "./PartTwo.module.css";
+
+function PartTwo() {
+    return (
+        <Card>
+        <div>
+        <div>
+        <selection>
+        <p>Groups</p>
+        </selection>
+        </div>
+        <div>
+        <selection>
+        <p>Events</p>
+        </selection>
+        </div>
+        <div>
+        <selection>
+        <p>Followed Hashtags</p>
+        </selection>
+        </div>
+        </div>
+        <hr />
+        <div>
+        <h2>Discover more</h2>
+        </div>
+        </Card>
+        )}
+        export default PartTwo
