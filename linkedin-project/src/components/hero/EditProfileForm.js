4b7bf5ff7dbc43b8d207697fd7479ed0bbb4a41a
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-05 13:34:41 +0100

    added form

--- linkedin-project/src/components/hero/EditProfileForm.js
+++ linkedin-project/src/components/hero/EditProfileForm.js
@@ -4,26 +4,49 @@ import classes from "./EditProfileForm.module.css";
 
 export default function EditProfileForm() {
     return (
-        <form className={classes.form}>
-            <div className={classes.control}>
-                <label htmlFor="title">Post Title</label>
-                <input type="text" id="title" required />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="image">Post Image</label>
-                <input type="url" id="image" required />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="address">Address</label>
-                <input type="text" id="address" required />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="description">Descrioption</label>
-                <textarea id="description" rows="5" required></textarea>
-            </div>
-            <div className={classes.actions}>
-                <button>Add Post</button>
-            </div>
-        </form>
+        <section className={classes.main}>
+            <h3>Edit Info</h3>
+            <hr />
+            <container>
+                <h6>
+                    <small>* indicates required</small>{" "}
+                </h6>
+                <form className={classes.form}>
+                    <div className={classes.control}>
+                        <label htmlFor="name">First name*</label>
+                        <input type="text" id="userFirstName" required />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="userinputLastName">Last name*</label>
+                        <input type="text" id="userInputLastName" required />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="userInputEmail">Email*</label>
+                        <input type="email" id="userInputEmail" required />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="address">Title</label>
+                        <input type="text" id="title" required />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="userInputBio">Bio</label>
+                        <textarea
+                            id="userInputBio"
+                            rows="5"
+                            required
+                        ></textarea>
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="userInputArea">Area*</label>
+                        <input type="text" id="userInputArea" required />
+                    </div>
+
+                    <hr />
+                </form>
+                <div className={classes.actions}>
+                    <button>Add Post</button>
+                </div>
+            </container>
+        </section>
     );
 }
