da9d4c03b351931f52b3b78335bfad74bb2b7840
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-05-05 16:03:06 +0300

    finished 2 cards visuals

--- linkedin-project/src/components/SmolProfile/PartTwo.jsx
+++ linkedin-project/src/components/SmolProfile/PartTwo.jsx
@@ -4,27 +4,28 @@ import classes from "./PartTwo.module.css";
 
 function PartTwo() {
     return (
-        <Card>
+        <Card className={classes.CardEdit}>
         <div>
-        <div>
+        <div className={classes.DivPadding}>
         <selection>
-        <p>Groups</p>
+        <p className={classes.LinkText}>Groups</p>
         </selection>
         </div>
-        <div>
+        <div className={classes.DivPadding2}>
         <selection>
-        <p>Events</p>
+        <p className={classes.LinkText}>Events</p>
         </selection>
+        <span className={classes.Pluss}>+</span>
         </div>
-        <div>
+        <div className={classes.DivPadding}>
         <selection>
-        <p>Followed Hashtags</p>
+        <p className={classes.LinkText}>Followed Hashtags</p>
         </selection>
         </div>
         </div>
         <hr />
         <div>
-        <h2>Discover more</h2>
+        <h2 className={classes.Discover}>Discover more</h2>
         </div>
         </Card>
         )}
