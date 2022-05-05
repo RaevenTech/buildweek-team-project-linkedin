cbe90e8137d8204455ed6d3f903b1078e8dad09b
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-05 15:57:02 +0100

    added api "POSTING"

--- linkedin-project/src/components/hero/EditProfileForm.js
+++ linkedin-project/src/components/hero/EditProfileForm.js
@@ -1,8 +1,36 @@
 import React from "react";
-
+import { useRef } from "react";
 import classes from "./EditProfileForm.module.css";
 
-export default function EditProfileForm() {
+export default function EditProfileForm(props) {
+    const firstNameInputRef = useRef();
+    const lastNameInputRef = useRef();
+    const emailInputRef = useRef();
+    const areaInputRef = useRef();
+    const titleInputRef = useRef();
+    const bioInputRef = useRef();
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        const userFormNameInput = firstNameInputRef.current.value;
+        const userFormLastNameInput = lastNameInputRef.current.value;
+        const userFormEmailInput = emailInputRef.current.value;
+        const userFormAreaInput = areaInputRef.current.value;
+        const userFormTitleInput = titleInputRef.current.value;
+        const userFormBioInput = bioInputRef.current.value;
+
+        const inputDataOject = {
+            name: userFormNameInput,
+            surname: userFormLastNameInput,
+            email: userFormEmailInput,
+            area: userFormAreaInput,
+            title: userFormTitleInput,
+            bio: userFormBioInput,
+        };
+        props.onSave(inputDataOject);
+    }
+
     return (
         <section className={classes.main}>
             <h3>Edit Info</h3>
@@ -11,40 +39,66 @@ export default function EditProfileForm() {
                 <h6>
                     <small>* indicates required</small>{" "}
                 </h6>
-                <form className={classes.form}>
+                <form className={classes.form} onSubmit={submitHandler}>
                     <div className={classes.control}>
                         <label htmlFor="name">First name*</label>
-                        <input type="text" id="userFirstName" required />
+                        <input
+                            type="text"
+                            id="userFirstName"
+                            ref={firstNameInputRef}
+                            required
+                        />
                     </div>
                     <div className={classes.control}>
-                        <label htmlFor="userinputLastName">Last name*</label>
-                        <input type="text" id="userInputLastName" required />
+                        <label htmlFor="userInputLastName">Last name*</label>
+                        <input
+                            type="text"
+                            id="userInputLastName"
+                            ref={lastNameInputRef}
+                            required
+                        />
                     </div>
                     <div className={classes.control}>
                         <label htmlFor="userInputEmail">Email*</label>
-                        <input type="email" id="userInputEmail" required />
+                        <input
+                            type="email"
+                            id="userInputEmail"
+                            ref={emailInputRef}
+                            required
+                        />
                     </div>
                     <div className={classes.control}>
-                        <label htmlFor="address">Title</label>
-                        <input type="text" id="title" required />
+                        <label htmlFor="userInputTitle">Title</label>
+                        <input
+                            type="text"
+                            id="userInputTitle"
+                            ref={titleInputRef}
+                            required
+                        />
                     </div>
                     <div className={classes.control}>
                         <label htmlFor="userInputBio">Bio</label>
                         <textarea
                             id="userInputBio"
+                            ref={bioInputRef}
                             rows="5"
                             required
                         ></textarea>
                     </div>
                     <div className={classes.control}>
                         <label htmlFor="userInputArea">Area*</label>
-                        <input type="text" id="userInputArea" required />
+                        <input
+                            type="text"
+                            id="userInputArea"
+                            ref={areaInputRef}
+                            required
+                        />
                     </div>
 
                     <hr />
                 </form>
                 <div className={classes.actions}>
-                    <button>Add Post</button>
+                    <button>Save</button>
                 </div>
             </container>
         </section>
--- linkedin-project/src/components/hero/MyProfile.js
+++ linkedin-project/src/components/hero/MyProfile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { MdModeEditOutline } from "react-icons/md";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { Jumbotron, Button, Modal } from "react-bootstrap";
 import classes from "./MyProfile.module.css";
 import ProfileButtons from "./ProfileButtons";
@@ -11,6 +11,20 @@ export default function MyProfile() {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    function addEditInfoFromObjectDataHandler(inputDataOject) {
+        fetch("https://striveschool-api.herokuapp.com/api/profile/", {
+            method: "POST",
+            body: JSON.stringify(inputDataOject),
+            headers: {
+                authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjZmYWI2ZjE3YzRlMDAwMTVkN2EwODEiLCJpYXQiOjE2NTE0ODU1NTIsImV4cCI6MTY1MjY5NTE1Mn0.6oiKKnrkIJeweRS_PUJB__l7YKgogrSnme8NbyUpz4Q",
+                "Content-Type": "application/json",
+            },
+        }).then(() => {
+            Navigate("/");
+        });
+    }
 
     return (
         <div>
@@ -43,7 +57,9 @@ export default function MyProfile() {
                         <Modal.Title>Modal heading</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <EditProfileForm />
+                        <EditProfileForm
+                            onSave={addEditInfoFromObjectDataHandler}
+                        />
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleClose}>
