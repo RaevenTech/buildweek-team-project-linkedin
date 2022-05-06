a62b29d41ce7aed93df206e64dab7101b888b31d
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-05-06 12:06:25 +0300

    card without styling and icons

--- linkedin-project/src/components/PostBase/PostBase.jsx
+++ linkedin-project/src/components/PostBase/PostBase.jsx
@@ -5,21 +5,56 @@ import classes from "./PostBase.module.css";
 
 function PostBase() {
     return (
-<Card>
+<Card className={classes.Post}>
     <div>
         <Row>
-            <Col md={11}>
-                <div>
+            <Col md={2}>
             <img className={classes.PosterPfp} src="https://via.placeholder.com/80" alt={"..."}/>
-<h6>Poster Name</h6>
+            </Col>
+            <Col md={9}>
+            <h6>Users Name</h6>
 <p>9999 followers</p>
 <p>1w <span><FaGlobeAmericas /></span></p>
-</div>
-            </Col>
+                </Col>
             <Col md={1}>
+                <p>...</p>
             </Col>
          </Row>
     </div>
+    <div>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut veniam,
+             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    </div>
+    <div>
+    <img className={classes.postsImage} src="https://via.placeholder.com/600" alt={"..."}/>
+    </div>
+    <div>
+        <Row>
+            <Col md={9}>
+                <p>123</p>
+            </Col>
+            <Col md={2}>
+            <p>7 comments</p>
+            </Col>
+            <Col md={1}>
+               <p> 5 shares</p>
+            </Col>
+        </Row>
+    </div>
+    <hr />
+    <div>
+        <Row>
+        <Col md={3}>
+            </Col>
+            <Col md={3}>
+            </Col>
+            <Col md={3}>
+            </Col>
+            <Col md={3}>
+            </Col>
+            </Row>
+            </div>
 </Card>
     )
 }
