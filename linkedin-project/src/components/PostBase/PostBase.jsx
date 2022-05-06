4fc2e9d193bc5920805cc1798038149a0aab7dbf
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-05-06 12:35:14 +0300

    CSS formatting go brrrr

--- linkedin-project/src/components/PostBase/PostBase.jsx
+++ linkedin-project/src/components/PostBase/PostBase.jsx
@@ -6,22 +6,22 @@ import classes from "./PostBase.module.css";
 function PostBase() {
     return (
 <Card className={classes.Post}>
-    <div>
+    <div className={classes.DivPad}>
         <Row>
             <Col md={2}>
             <img className={classes.PosterPfp} src="https://via.placeholder.com/80" alt={"..."}/>
             </Col>
-            <Col md={9}>
-            <h6>Users Name</h6>
-<p>9999 followers</p>
-<p>1w <span><FaGlobeAmericas /></span></p>
+            <Col className={classes.DivPad} md={9}>
+            <a className={classes.UserName}>Users Name</a>
+<p className={classes.MutedSmol}>9999 followers</p>
+<p className={classes.MutedSmol}>1w <span><FaGlobeAmericas /></span></p>
                 </Col>
-            <Col md={1}>
-                <p>...</p>
+            <Col className={classes.DivPad} md={1}>
+                <p className={classes.TheDots}>...</p>
             </Col>
          </Row>
     </div>
-    <div>
+    <div className={classes.DivPad}>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
             ut labore et dolore magna aliqua. Ut veniam,
              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
@@ -29,21 +29,21 @@ function PostBase() {
     <div>
     <img className={classes.postsImage} src="https://via.placeholder.com/600" alt={"..."}/>
     </div>
-    <div>
+    <div className={classes.DivPad}>
         <Row>
-            <Col md={9}>
-                <p>123</p>
+            <Col md={8}>
+                <p className={classes.MutedSmol}>123</p>
             </Col>
             <Col md={2}>
-            <p>7 comments</p>
+            <p className={classes.MutedSmol}>7 comments</p>
             </Col>
-            <Col md={1}>
-               <p> 5 shares</p>
+            <Col md={2}>
+               <p className={classes.MutedSmol}>5 shares</p>
             </Col>
         </Row>
     </div>
-    <hr />
-    <div>
+    <hr className={classes.LineNoPad} />
+    <div className={classes.DivPad}>
         <Row>
         <Col md={3}>
             </Col>
